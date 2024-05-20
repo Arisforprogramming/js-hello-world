8c783a8ea47402048170f79edb17bc61f24d01ab
arissaracode <133154320+Arisforprogramming@users.noreply.github.com>
2024-05-19 23:37:55 -0500

    adding hello word and play with messages

--- hello.js
+++ hello.js
@@ -4,7 +4,8 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Hello, World! Arissara is the programmer"+ "?")
+console.log(2+5)
 
 /*  
 Next, PLAY! 
